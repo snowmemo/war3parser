c2f45d20f7c36ac6c7df3419e4afc5dcea885f9a
Wesley Yang <48174882+wesleyel@users.noreply.github.com>
2025-01-18 21:49:38 +0800

    feat: add width and height fields to WasmImage for improved image metadata
    - Enhanced the WasmImage struct by adding width and height fields to store image dimensions.
    - Updated the From<War3Image> implementation to extract and assign these dimensions during conversion, improving the metadata handling for images.

--- crates/wasm/src/types/wasm_image.rs
+++ crates/wasm/src/types/wasm_image.rs
@@ -5,14 +5,20 @@ use wasm_bindgen::prelude::wasm_bindgen;
 #[wasm_bindgen(getter_with_clone)]
 pub struct WasmImage {
     pub data_url: String,
+    pub width: u32,
+    pub height: u32,
     pub filename: String,
 }
 
 impl From<War3Image> for WasmImage {
     fn from(image: War3Image) -> Self {
+        let width = image.data.width();
+        let height = image.data.height();
         let war3image_base64 = War3ImageBase64::try_from(image).unwrap();
         Self {
             data_url: war3image_base64.data,
+            width,
+            height,
             filename: war3image_base64.filename,
         }
     }
